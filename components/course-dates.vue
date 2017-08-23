62c2f2d062e79881f3e1de672f7a76dd43444816
Marius Rickenbacher <marius.rickenbacher@ricardo.ch>
2017-08-24 01:51:17 +0200

    courses part 1

---
+++ components/course-dates.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Kursdaten</h3>
+  <ul>
+    <li v-for="date in dates">{{date}}</li>
+  </ul>
+
+</template>
+
+<script>
+  export default {
+    props: ['dates']
+  }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ store/courses.js
@@ -0,0 +1,17 @@
+'use strict'
+
+export default {
+  state: {
+    dates: [
+      {date: new Date(2017, 8, 4)},
+      {date: new Date(2017, 9, 2)},
+      {date: new Date(2017, 10, 13)},
+      {date: new Date(2017, 11, 4)},
+      {date: new Date(2018, 0, 8)},
+      {date: new Date(2018, 1, 5)},
+      {date: new Date(2018, 2, 5)},
+      {date: new Date(2018, 3, 9)},
+      {date: new Date(2018, 4, 7)}
+    ]
+  }
+}
