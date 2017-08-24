c82614bd558c8514176139f3d6f7d2c6b5f31f93
gwildu <gwildu@gwildu.ch>
2017-08-24 02:18:13 +0200

    Open Sans font added

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'bildhauerei-rickenbacher.ch website' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700' }
     ]
   },
   /*
