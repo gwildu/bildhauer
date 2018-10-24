5e7e7b5005bca5e3cc1d1a4fb7df60a248fdd3c8
gwildu <gwildu@gwildu.ch>
2018-10-24 22:52:45 +0200

    remove SPA features and add dist folder plus serveral other fixes

---
+++ dist/precache-manifest.867b7db255fdaedeb6a6dfce14dbcdcb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "a706c63824da1caf6402f9a17c7759e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9bf631a1cf0ff83d8af5dddf48f7774",
+    "url": "/downloads/kunst-zu-erhalten.pdf"
+  },
+  {
+    "revision": "17d9b1b7274fb9d886d1",
+    "url": "/js/chunk-vendors.b629790a.js"
+  },
+  {
+    "revision": "9464bf33e1d0a575769cf08302d4713c",
+    "url": "/downloads/pressespiegel.pdf"
+  },
+  {
+    "revision": "e0d2c4556452ad9b0437",
+    "url": "/js/decorationInteriorGarden.8c1c0fdf.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4a6874123b45164a2603",
+    "url": "/js/gravemarkers.acbc3de1.js"
+  },
+  {
+    "revision": "1183e08dcd59eade5d39e88f6ffe7b3a",
+    "url": "/misc/under-construction.jpg"
+  },
+  {
+    "revision": "74d9fdb02827ca787ce6",
+    "url": "/js/restorations.f5e26540.js"
+  },
+  {
+    "revision": "93f53e79391dc0de07c0",
+    "url": "/js/app.25675c75.js"
+  },
+  {
+    "revision": "923b0089893aa36a5978a049020785ec",
+    "url": "/downloads/ein-zeichen-setzen.pdf"
+  },
+  {
+    "revision": "5103807bdfadffb29ece9e4f553cecbe",
+    "url": "/downloads/dokumentation-sitzende-frau.pdf"
+  },
+  {
+    "revision": "e4b659e2d6ccb0e04fef58364a222b12",
+    "url": "/downloads/dokumentation-kirchgasse-27.pdf"
+  },
+  {
+    "revision": "2841670d73dfc8dd6c2f46c5e8ad8454",
+    "url": "/downloads/dokumentation-galluspfarrei-oberuzwil.pdf"
+  },
+  {
+    "revision": "4c7840a7f9735b9b3685e0201aab513f",
+    "url": "/downloads/dokumentation-dorfbrunnen.pdf"
+  },
+  {
+    "revision": "74d9fdb02827ca787ce6",
+    "url": "/css/restorations.6c2419b5.css"
+  },
+  {
+    "revision": "4a6874123b45164a2603",
+    "url": "/css/gravemarkers.6c2419b5.css"
+  },
+  {
+    "revision": "e0d2c4556452ad9b0437",
+    "url": "/css/decorationInteriorGarden.6c2419b5.css"
+  },
+  {
+    "revision": "93f53e79391dc0de07c0",
+    "url": "/css/app.a3d2379d.css"
+  }
+];
