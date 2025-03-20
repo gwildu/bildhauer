320a05510fa97a45bbad37ec3d36e37a8688fc68
@gwildu <gwildu@gmx.net>
2025-03-20 21:40:41 +0100

    add job offer 1

--- src/entrypoints/job-offer/index.tsx
+++ src/entrypoints/job-offer/index.tsx
@@ -1,14 +1,21 @@
 import { Heading } from "../../packages/typography/heading";
-import { Paragraph } from "../../packages/typography/paragraph";
 import { DefaultInnerLayout } from "../../layout/default/inner-layout/inner-layout";
 import { PageHead } from "../../packages/seo/page-head";
+import { TextListUnordered } from "../../packages/list/textList";
+import { Link } from "../../link";
 
 export const JobOffer = () => (
   <DefaultInnerLayout pageHead={pageHead}>
     <Heading markupLevel={1} stylingLevel={"MAIN_HEADING"}>
       <strong>Stellenangebote</strong>
     </Heading>
-    <Paragraph>Momentan haben wir keine offenen Stellen...</Paragraph>
+    <TextListUnordered>
+      <li>
+        <Link href={"/job-offer/1"}>
+          Steinbildhauer/in 50-100% in der Grabmal Herstellung
+        </Link>
+      </li>
+    </TextListUnordered>
   </DefaultInnerLayout>
 );
 
--- src/job-offer/job-offer.tsx
+++ src/job-offer/job-offer.tsx
@@ -8,10 +8,10 @@ export const JobOffer: FC = () => {
   return (
     <>
       <Paragraph>
-        Zur Verstärkung suchen wir nach Vereinbarung eine/n
+        Zur Verstärkung suchen wir per sofort oder nach Vereinbarung eine/n
       </Paragraph>
       <Heading markupLevel={1} stylingLevel={"MAIN_HEADING"}>
-        <strong>Steinbildhauer/in 60-100%</strong> in der Grabmal Herstellung
+        <strong>Steinbildhauer/in 50-100%</strong> in der Grabmal Herstellung
       </Heading>
       <Heading markupLevel={2} stylingLevel={"SUB_HEADING_1"}>
         Ihre Aufgaben umfassen:
@@ -37,7 +37,7 @@ export const JobOffer: FC = () => {
         </li>
       </TextListUnordered>
       <Heading markupLevel={2} stylingLevel={"SUB_HEADING_1"}>
-        Profil / Muss-Kriterien:
+        Profil /&nbsp;Muss-Kriterien:
       </Heading>
       <TextListUnordered>
         <li>
