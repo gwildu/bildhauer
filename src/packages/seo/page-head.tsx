b5be1a603212df90cb12b0d285025d4274f41ea5
@gwildu <gwildu@gmx.net>
2025-04-03 23:33:00 +0200

    adds working image for job offer pages

--- src/packages/seo/page-head.tsx
+++ src/packages/seo/page-head.tsx
@@ -3,13 +3,15 @@ import { FC } from "react";
 
 interface IPageHead {
   title: string;
+  canonical?: string;
 }
 
-export const PageHead: FC<IPageHead> = ({ title }) => {
+export const PageHead: FC<IPageHead> = ({ title, canonical }) => {
   return (
     <Head>
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
+      {canonical && <link rel="canonical" href={canonical} />}
     </Head>
   );
 };
